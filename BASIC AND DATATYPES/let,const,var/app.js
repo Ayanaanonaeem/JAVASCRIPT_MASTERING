83f66df48e4600eda8ee9f76954b2089e14ffb41
Ayanaanonaeem <ayanaano5@gmail.com>
2025-02-18 17:33:08 +0500

    file create

---
+++ BASIC AND DATATYPES/let,const,var/app.js
@@ -0,0 +1,26 @@
+// let a=3
+// let a=4
+
+// this give an error kynke hum variable ko change krte hain to let nhi lagate//
+
+let b=3
+b=4
+
+
+// var main hum var laga kr hi change krsakte hain variable ko 
+var c=4
+var c=5;
+
+
+// const main hum change nhi krsakte 
+
+const  d=8;
+ d=9;
+
+ console.log(d);
+ 
+// this give an error n browser because const element cannot change
+
+// block scope ko hum redeclare nhi krsakte 
+// global scope ko redeclare krsakte hain jese ke var  
+
