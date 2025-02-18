71bfee0e05223cfff341dace37db1b34f692d618
Ayanaanonaeem <ayanaano5@gmail.com>
2025-02-18 18:02:03 +0500

    file create

---
+++ BASIC AND DATATYPES/For loops/app.js
@@ -0,0 +1,30 @@
+// printing number from 1 to 100//
+
+for(var i=0;i<=100;i++){
+
+    console.log(i);
+    
+}
+
+// now printing the table of any number //
+
+for(var i=0; i<=10; i++){
+    console.log(`4 x ${i} = ${4*i}`);
+    
+}
+
+
+var array=["mango","orange","banana","apple","peach","guava"]
+
+for(var i=0; i<array.length;i++){
+    console.log(array[i]);
+    
+}
+
+// for of loop //
+
+for (const value of array) {
+    console.log(value);
+    
+}
+
