5aa6d49d6993b81c37b8b82b88568d48f2471f77
Ayanaanonaeem <ayanaano5@gmail.com>
2025-02-20 16:58:48 +0500

    file create

---
+++ Arrays and Object/Array/Push and pop/app.js
@@ -0,0 +1,19 @@
+//888888888888888888 PUSH METHOD 88888888888888888888888888888//
+
+// 1.push ek method hai array ka jis ki wjh se hum array ke end main kuch bhi add karwasakte hain 
+
+var array=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+array.push(14)
+
+console.log(array);
+
+
+
+// 888888888888888   POP METH0D 8888888888888888888888888//
+
+// 2.pop bhi array ka ek method hai jiski wjh se hum array ke end se kuch bhi nikal sakte hain
+
+var newarr=array.pop()
+
+console.log(newarr);
