03065ac68eadb10692484be0d39ffb1da4c66c77
Ayanaanonaeem <ayanaano5@gmail.com>
2025-02-24 22:28:16 +0500

    file create

---
+++ Arrays and Object/Array/Sort and Reverse/app.js
@@ -0,0 +1,11 @@
+// sort method array ko chote se bare main sort krdeta hai
+
+var arr=[1,7,3,4,5,8,1,2,9]
+arr.sort()
+console.log(arr);
+
+
+// reverse method reverse the array
+var arr2=[1,2,3,4,5,6,7,8,9,10]
+arr2.reverse()
+console.log(arr2);
