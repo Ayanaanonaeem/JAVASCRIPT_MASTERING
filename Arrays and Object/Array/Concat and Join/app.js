ece473db197e6e70230d908321a26757fa8f8611
Ayanaanonaeem <ayanaano5@gmail.com>
2025-02-25 17:40:16 +0500

    file create

---
+++ Arrays and Object/Array/Concat and Join/app.js
@@ -0,0 +1,14 @@
+// concat is  used is to combine two array 
+
+var arr=[1,2,3,4,5]
+var arr1=[6,7,8,9,10]
+
+var newarr=arr.concat(arr1)
+
+console.log(newarr);
+
+
+// join is used is convert the array into string
+
+var newarr=arr.join()
+console.log(newarr);
---
+++ Arrays and Object/Array/Indexof and LastIndexOf/app.js
@@ -0,0 +1,9 @@
+// indexof is used to find the index of some random number
+
+var arr=[1,2,3,4,5]
+var arr1=[6,7,8,9,10]
+
+console.log(arr.indexOf(4));
+
+// lastindexof is same as indexof but it start finding from last
+console.log(arr1.lastIndexOf(10));
