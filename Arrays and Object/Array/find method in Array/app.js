60635f25275ec6d43da75ec0dea26cbaf6967778
Ayanaanonaeem <ayanaano5@gmail.com>
2025-02-28 17:25:35 +0500

    file create

---
+++ Arrays and Object/Array/find method in Array/app.js
@@ -0,0 +1,33 @@
+const employees = [
+    {
+      id: 1,
+      name: 'John Doe',
+      position: 'Software Engineer',
+      salary: 80000
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      position: 'Product Manager',
+      salary: 95000
+    },
+    {
+      id: 3,
+      name: 'Sam Brown',
+      position: 'Designer',
+      salary: 70000
+    },
+    {
+      id: 4,
+      name: 'Emily Davis',
+      position: 'QA Tester',
+      salary: 60000
+    }
+  ];
+
+  var employeesfind=employees.find((user)=>{
+    return user.name=='Emily Davis'
+  })
+
+  console.log(employeesfind);
+  
