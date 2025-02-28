9b9a196f1a80b13b55a59eea33941c044e8318be
Ayanaanonaeem <ayanaano5@gmail.com>
2025-02-28 17:19:09 +0500

    file create

---
+++ Arrays and Object/Object/find method/app.js
@@ -0,0 +1,25 @@
+// const users1 = [
+//     { id: 1, name: 'Alice', age: 25 },
+//     { id: 2, name: 'Bob', age: 30 },
+//     { id: 3, name: 'Charlie', age: 35 }
+//   ];
+
+//   let finduser=users.find((user)=>{
+     
+//     return user.id==2
+    
+//   })
+
+//   console.log(finduser);
+  
+
+  const users = [
+    { id: 1, name: 'Alice', age: 25 },
+    { id: 2, name: 'Bob', age: 30 },
+    { id: 3, name: 'Charlie', age: 35 }
+  ];
+
+  let finding_user=users.find(user=>user.name=="Bob")
+
+  console.log(finding_user);
+  
