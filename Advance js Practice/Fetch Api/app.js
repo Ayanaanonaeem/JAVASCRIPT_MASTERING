8c6d4fd95ad3a2f6697f2024c6100119996d6b62
Ayanaanonaeem <ayanaano5@gmail.com>
2025-03-14 23:50:08 +0500

    file create

--- Advance js Practice/Fetch Api/app.js
+++ Advance js Practice/Fetch Api/app.js
@@ -3,3 +3,14 @@ console.log("hello ");
 fetch('https://fakestoreapi.com/products')
 .then((res)=>res.json())
 .then((data)=>console.log(data))
+
+
+
+fetch('https://fakestoreapi.com/products')
+.then((res)=>{
+   return res.json()
+})
+.then((data)=>{
+   return console.log(data);
+    
+})
