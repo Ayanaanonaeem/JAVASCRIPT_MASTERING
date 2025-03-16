fd169198d39b9c7347ce04fbbf934ee4456e80fe
Ayanaanonaeem <ayanaano5@gmail.com>
2025-03-17 04:27:46 +0500

    file create

--- Advance js Practice/Spread operator/app.js
+++ Advance js Practice/Spread operator/app.js
@@ -4,3 +4,33 @@
 // ye bhi three dot laga kr use hota hai
 
 
+let obj={
+    name:"Ayan",
+    father_name:"Naeem",
+    education:"Undergraduate",
+    Salary:1200000
+}
+
+let obj2={
+    ...obj
+
+}
+
+let obj3=obj2
+obj3.Salary=1300000
+
+console.log(obj2);
+console.log(obj);
+console.log(obj3);
+
+var arr=[1,2,3,4,5,6,7,8,9,10]
+
+var[one,two,three,four,five]=arr
+
+console.log(one);
+console.log(two);
+console.log(three);
+
+
+
+
