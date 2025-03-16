da2abe90df4eaefc2138c4ebd6b73229bd41beb2
Ayanaanonaeem <ayanaano5@gmail.com>
2025-03-17 04:48:19 +0500

    file create

---
+++ Advance js Practice/Destructuring/app.js
@@ -0,0 +1,20 @@
+// destructuring ekk method hai jisse se hum obj ki properties or array ki valuess ko simple
+// variable main tor sakte hain aur use kahin bhi access krsakte hain 
+
+let obj={
+    name:"Ayan",
+    father_name:"Naeem",
+    education:"Undergraduate",
+    Salary:1200000
+}
+
+let{name,father_name,education}=obj
+
+console.log(name);
+console.log(father_name);
+console.log(education);
+
+
+var arr=[1,2,3,4,5,6,7,8,9,10]
+
+let{one,two,three}
