44e4cd590a2d06f26b284ee6ffb4065c603587b8
Ayanaanonaeem <ayanaano5@gmail.com>
2025-04-26 01:32:46 +0500

    file create

--- React_js/bgchanger/src/App.jsx
+++ React_js/bgchanger/src/App.jsx
@@ -1,32 +1,37 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(15)
-   var plus=()=>{
-    setCount((prevcount)=>prevcount+1)
-    setCount((prevcount)=>prevcount+1)
-    setCount((prevcount)=>prevcount+1)
-    setCount((prevcount)=>prevcount+1)
-    setCount((prevcount)=>prevcount+1)
- 
-    
-   }
-   var minus=()=>{
-    setCount(count-1)
-   }
-  return (
-    <>
-    <br />
-    <br /><br />
-     <div className='text-center'>
-      <h1 className='text-8xl'>{count}</h1>
-      <button onClick={plus} className='bg-blue-600 rounded-2xl px-4 border border-black mr-7'>PLUS</button>
-      <button onClick={minus} className='bg-blue-600 rounded-2xl px-4 border border-black'>Minus</button>
-      </div>
-    </>
-  )
+	const [color, setcolor] = useState("yellow");
+	return (
+		<>
+			<div className="w-full h-screen" style={{ background:color }}>
+				<div className="fixed bottom-4 flex flex-wrap justify-center w-full">
+					<div className="flex flex-wrap justify-center w-9/12 border-4 border-black gap-4 rounded-2xl ">
+						<button className="bg-red-600 rounded-2xl px-6 py-2 border border-black shadow-black my-3"
+            onClick={()=>{
+              setcolor("red")
+            }}>
+							Red
+						</button>
+						<button className="bg-blue-600 rounded-2xl px-6 py-2 border border-black shadow-black my-3 transition-all delay-2000"
+             onClick={()=>{
+              setcolor("blue")
+            }}>
+							Blue
+						</button>
+						<button className="bg-green-600 rounded-2xl px-6 py-2 border border-black shadow-black my-3"
+             onClick={()=>{
+              setcolor("green")
+            }}>
+							Green
+						</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
