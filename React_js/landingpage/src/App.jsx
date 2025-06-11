4d1d9400162949fa04ad228b2c5f766ad92c3334
Ayanaanonaeem <ayanaano5@gmail.com>
2025-06-11 05:58:16 +0500

    file create

--- React_js/landingpage/src/App.jsx
+++ React_js/landingpage/src/App.jsx
@@ -5,18 +5,50 @@ import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Testimonials from "./components/testimonials";
 import Footer from "./components/Footer";
+import { useState } from "react";
 
 function App() {
-  const{header,hero,features,testimonials}=landingPageData
-  
-  
+	const { header, hero, features, testimonials } = landingPageData;
+    const [username,setusername]=useState("Ayan")
+	const [counter,setcounter]=useState(0)
+	const[like,setlike]=useState(false)
+	 console.log(like);
+	// const handleusername=(()=>{
+	// 	setusername("Ayan Naeem")	
+	// })
+	const handleinput=((e)=>{
+      setusername(e.target.value)
+	})
+	// const handlecounter=(()=>{
+	// 	setcounter(counter+1)
+	// })
+	console.log(username);
+	
 	return (
 		<>
-			<Header header={header}/>
-			<Hero hero={hero}/>
-      <Features features={features}/>
-      <Testimonials testimonials={testimonials}/>
-      <Footer header={header}/>
+		{/* <h1>{counter}</h1>
+		<button onClick={handlecounter} className="border border-purple-500 bg-amber-300">update counter</button> */}
+        <p>{username}</p>
+		<input type="text" 
+		value={username}
+		onChange={handleinput}
+		className="border border-black"
+		/>
+		<input type="checkbox" 
+		checked={like}
+		onChange={(e)=>{
+          setlike(e.target.checked)
+		 
+	 console.log(like);
+		  
+		}}
+		/>
+		{/* <button onClick={handleusername} className="border border-purple-500 bg-amber-300">handle username</button> */}
+		{/* <Header header={header}/>
+		<Hero hero={hero}/>
+        <Features features={features}/>
+        <Testimonials testimonials={testimonials}/>
+        <Footer header={header}/> */}
 		</>
 	);
 }
